f1b69433ac76f4f1ad6cf0f3b988b754d57bdae5
Dieter Doman <dieter@jemstep.com>
2019-08-21 11:40:49 +0200

    Split state into indepenent useStates.

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,8 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-    const [state, setState] = useState({title: "", amount: ""});
+    const [titleState, setTitleState] = useState("");
+    const [amountState, setAmountState] = useState("");
 
     const submitHandler = event => {
         event.preventDefault();
@@ -17,22 +18,13 @@ const IngredientForm = React.memo(props => {
                 <form onSubmit={submitHandler}>
                     <div className="form-control">
                         <label htmlFor="title">Name</label>
-                        <input type="text" id="title" value={state.title} onChange={
-                            event => {
-                                const newTitle = event.target.value;
-                                setState(prevState => ({...prevState, title: newTitle}));
-                            }
-                        }/>
+                        <input type="text" id="title" value={titleState}
+                               onChange={event => setTitleState(event.target.value)}/>
                     </div>
                     <div className="form-control">
                         <label htmlFor="amount">Amount</label>
-                        <input type="number" id="amount" value={state.amount}
-                               onChange={
-                                   event => {
-                                       const newAmount = event.target.value;
-                                       setState(prevState => ({...prevState, amount: newAmount}))
-                                   }
-                               }/>
+                        <input type="number" id="amount" value={amountState}
+                               onChange={event => setAmountState(event.target.value)}/>
                     </div>
                     <div className="ingredient-form__actions">
                         <button type="submit">Add Ingredient</button>
