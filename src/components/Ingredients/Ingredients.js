ddee395088a0581a6aa9cf380467278b26d85a95
Dieter Doman <dieter@jemstep.com>
2019-08-21 13:08:41 +0200

    Added statement to ingredients using useState.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,29 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import IngredientList from "./IngredientList";
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 
-function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+const Ingredients = () => {
+    const [ingredients, setIngredients] = useState([]);
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
-}
+    const addIngredientsHandler = ingredient => {
+        setIngredients(prevState => [...prevState, {id: Math.random().toString(), ...ingredient}]);
+    };
+
+    const removeIngredientHandler = id => {
+        setIngredients(prevState => prevState.filter(ingredient => ingredient.id !== id));
+    };
+
+    return (
+        <div className="App">
+            <IngredientForm onAddIngredient={addIngredientsHandler}/>
+
+            <section>
+                <Search/>
+                <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler}/>
+            </section>
+        </div>
+    );
+};
 
 export default Ingredients;
