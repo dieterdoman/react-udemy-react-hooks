edf207a6758dbb264802d143956cfed1577c27d0
Dieter Doman <dieter@jemstep.com>
2019-08-21 13:29:57 +0200

    Added useEffect call to query server.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import IngredientList from "./IngredientList";
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,8 +6,32 @@ import Search from './Search';
 const Ingredients = () => {
     const [ingredients, setIngredients] = useState([]);
 
+    useEffect(() => {
+        fetch('https://react-hooks-9390e.firebaseio.com/ingredients.json').then(response => {
+            return response.json();
+        }).then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+                loadedIngredients.push({
+                    id: key,
+                    title: responseData[key].title,
+                    amount: responseData[key].amount
+                });
+                setIngredients(loadedIngredients);
+            }
+        });
+    }, []);
+
     const addIngredientsHandler = ingredient => {
-        setIngredients(prevState => [...prevState, {id: Math.random().toString(), ...ingredient}]);
+        fetch('https://react-hooks-9390e.firebaseio.com/ingredients.json', {
+            method: 'POST',
+            body: JSON.stringify(ingredient),
+            headers: {'Content-Type': 'application/json'}
+        }).then(response => {
+            return response.json();
+        }).then(responseData => {
+            setIngredients(prevState => [...prevState, {id: responseData.name, ...ingredient}]);
+        });
     };
 
     const removeIngredientHandler = id => {
